6aa2c5fc10e249abeddf3cb563893997a9c27de5
asnri <h5dy4mvk98@privaterelay.appleid.com>
2026-07-07 02:08:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ window.onload = () => {
   const prize = params.get("prize");
 
   let got = JSON.parse(localStorage.getItem("got") || "[]");
-
+  console.log("got =", got);
+  alert(JSON.stringify(got));
+  
   const button = document.getElementById("stampButton");
 
   // 保存済みスタンプ表示
